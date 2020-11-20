8353f654c1f028a8fc9c697681f100425a33ae0d
Giuseppe Di Pasquale <63881945+giuseppedipasquale@users.noreply.github.com>
2020-11-20 21:48:18 +0100

    Update test_01.py

--- test_01.py
+++ test_01.py
@@ -3,15 +3,19 @@ import functools
 from poliastro.core.propagation import func_twobody
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from nbkode import RungeKutta45, DOP853
 
-import nbkode
 
-t = [0, 24000]
-u0 = np.array([7600,0,0,0,8,0])
+tend = 100                     # Finish time [s]
 
-k = 398600
+t = [0, tend]                   # Time begin and end for scipy
+u0 = np.array([7600,0,0,0,8,0]) # Initial condition vector (for both)
+
+k = 398600                      # grav. param
+
 def ad(t0, u_, k_):
     return 0, 0, 0
+f = functools.partial(func_twobody,k=k,ad=ad,ad_kwargs={})
 
 def fsimpler(t, u):
     x, y, z, vx, vy, vz = u
@@ -19,32 +23,38 @@ def fsimpler(t, u):
     du = np.array([vx, vy, vz, -k * x / r3, -k * y / r3, -k * z / r3])
     return du
 
-#f = functools.partial(func_twobody,k=k,ad=ad,ad_kwargs={})
-
 print("start scipy")
-sol = solve_ivp(fsimpler, t, u0, atol = 1e-13, rtol = 1e-12)
+sol = solve_ivp(f, t, u0, atol = 1e-12, rtol = 1e-11 ,method = "DOP853")
 print("-----> end scipy")
+
+print("start nbkode")
+solver = RungeKutta45(func_twobody, 0, u0, atol = 1e-12, rtol = 1e-11, params = (k,ad,{}))
+tn = np.arange(0,tend,10)
+tn, yn = solver.run(tn)
+print("-----> end nbkode")
+
+
+# scipy solution
 T = sol.t
 X = sol.y[0,:]
 Y = sol.y[1,:]
 
-
-
-y0 = u0
-t0 = 0
-print("start nbkode")
-solver = nbkode.Euler(fsimpler, t0, y0)
-tn, yn = solver.run(T)
-print("-----> end nbkode")
-
+# nbkode solution
+TN = tn
 XN = yn[:,0]
 YN = yn[:,1]
 
+fct = 1
+
 fig, ax = plt.subplots(2,1)
-ax[0].plot(T/3600,X,'b')
-ax[0].plot(T/3600,XN,'r--')
+ax[0].plot(T/fct,X,'b')
+ax[0].plot(TN/fct,XN,'r--')
 ax[0].grid()
-ax[1].plot(T/3600,Y,'b')
-ax[1].plot(T/3600,YN,'r--')
+ax[1].plot(T/fct,Y,'b')
+ax[1].plot(TN/fct,YN,'r--')
 ax[1].grid()
+
+plt.figure()
+plt.plot(X,Y,'b')
+plt.plot(XN,YN,'r--')
 plt.show()
